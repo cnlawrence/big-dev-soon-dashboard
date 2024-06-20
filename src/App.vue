6e922745627b817bf69fb12524622e524ff5e544
cnlawrence1183 <1872389+cnlawrence@users.noreply.github.com>
2024-06-20 11:32:50 -0500

    Stubbing out a simple getData() call to dummyjson

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+
+let getData = () => {
+  return fetch("https://dummyjson.com/http/200")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
+getData();
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue + Tailwindcss ROBOTO" />
+  <button class="bg-black text-white" @click="getData()">Get Data</button>
 </template>
 
 <style scoped></style>
